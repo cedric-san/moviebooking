8302fc827dfb856bbee67036cf64af1e679e108f
cedric-san <motovfxsan@gmail.com>
2021-10-02 19:15:25 +0530

    2 files modified

--- src/SubNavBar/SubNavBar.js
+++ src/SubNavBar/SubNavBar.js
@@ -1,34 +1,37 @@
 import React from 'react';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
 
 const SubNavBar = (props) => {
   return (
-    <ul className="nav justify-content-center">
-      <li className="nav-item">
-        <a className="nav-link active" href="#">
-          Homes
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Movies
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Theatres
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Events
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Orders
-        </a>
-      </li>
-    </ul>
+    <BrowserRouter>
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">
+            Homes
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Movies
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Theatres
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Events
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Orders
+          </a>
+        </li>
+      </ul>
+    </BrowserRouter>
   );
 };
 
