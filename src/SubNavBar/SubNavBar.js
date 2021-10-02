43513d172f7f9810b91550f5506041f726e2d8cf
cedric-san <motovfxsan@gmail.com>
2021-10-02 22:58:11 +0530

    2 files modified

--- src/SubNavBar/SubNavBar.js
+++ src/SubNavBar/SubNavBar.js
@@ -2,24 +2,24 @@ import React from 'react';
 
 const SubNavBar = (props) => {
   return (
-    <ul class="nav justify-content-center">
-      <li class="nav-item">
-        <a class="nav-link active" href="#">
+    <ul className="nav justify-content-center">
+      <li className="nav-item">
+        <a className="nav-link active" href="#">
           Homes
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
+      <li className="nav-item">
+        <a className="nav-link" href="#">
           Movies
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
+      <li className="nav-item">
+        <a className="nav-link" href="#">
           Theatres
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
+      <li className="nav-item">
+        <a className="nav-link" href="#">
           Events
         </a>
       </li>
