faea2456e1dbc54ac37e62a7d6b97edebb52c15f
cedric-san <motovfxsan@gmail.com>
2021-10-03 11:28:01 +0530

    1 file modified

--- src/Cards.js
+++ src/Cards.js
@@ -1,13 +1,8 @@
 import './Cards.css';
 import React, { useState, useEffect } from 'react';
 import MovieCard from './MovieCards/MovieCard';
-import axios from 'axios';
-import Button from './Button/Button';
 
 const Cards = (props) => {
-  const onChangeMovieCard = () => {
-    setComingSoon(!comingSoon);
-  };
   return (
     <div>
       <h3 className="heading">Movies</h3>
