1049529320a4b2e548da1a493ea33666fa55c204
cedric-san <motovfxsan@gmail.com>
2021-10-06 23:37:18 +0530

    2 files modified

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Events = () => {
-  const style = {
-    textAlign: 'center',
-  };
-  return <h1 style={style}>Events Page</h1>;
+  const events = ['workshop', 'kids', 'comedy show', 'Talks', 'screening'];
+  const eventsCards = [
+    { eventName: 'Data Science', category: 'Tech', price: 'free' },
+    { eventName: 'on the spot', category: 'comedy', price: 'free' },
+    { eventName: 'Comedy Showcase', category: 'comedy', price: 'free' },
+  ];
+
+  return (
+    <div>
+      <h1>Events in Chennai</h1>
+      {events.map((events) => {
+        return (
+          <span
+            class="badge bg-primary"
+            style={{ margin: '5px', padding: '10px' }}
+          >
+            {events}
+          </span>
+        );
+      })}
+      <ul>
+        {eventsCards.map((movie, index) => {
+          return (
+            <li key={index}>
+              <div>
+                <div className="movieCards">
+                  <img style={{backgroundColor:'red',width:'200px'}}
+                    className="imgSize"
+                    src={}
+                    alt=""
+                  />
+                </div>
+
+                <div>
+                  <h4 style={{ width: '100px' }}>{movie.eventName}</h4>
+                  <p>{movie.category} </p>
+                  <p>{movie.price} </p>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Events;
--- src/Theatres/Theatres.js
+++ src/Theatres/Theatres.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Theatres = () => {
-  const style = {
-    textAlign: 'center',
-  };
-  return <h1 style={style}>Theatres Page</h1>;
+  return (
+    <div>
+      <h1>Theatres near</h1>
+      <p>Please set your location to see theaters</p>
+    </div>
+  );
 };
 
 export default Theatres;
