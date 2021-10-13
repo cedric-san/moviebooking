f0d88cc8827e27b6ad49bc3f56df513846befab8
cedric-san <motovfxsan@gmail.com>
2021-10-13 21:36:37 +0530

    2 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Error from './SubNavBar/Error';
 import Theatres from './Theatres/Theatres';
 import Events from './Events/Events';
 import Form from './Form/Form';
+import Store from './Store';
+import { Provider } from 'react-redux';
 
 import Banners from './Banners/Banners';
 export default function App() {
@@ -23,21 +25,24 @@ export default function App() {
     title: 'My Movie Booking',
   };
   const isBanner = false;
+
   return (
     <div>
-      <BrowserRouter>
-        <NavBar title={navbarhead} formChange={formChange} />
-        <SubNavBar />
-        {isBanner ? <Banners /> : null}
-        <Switch>
-          <Route path="/form" component={Form} />
-          <Route exact path="/movie" component={Cards} />
-          <Route exact path="/theatres" component={Theatres} />
-          <Route exact path="/event" component={Events} />
-          <Route path="/" component={Cards} />
-        </Switch>
-        <Footer title="footer menu" />
-      </BrowserRouter>
+      <Provider store={Store}>
+        <BrowserRouter>
+          <NavBar title={navbarhead} formChange={formChange} />
+          <SubNavBar />
+          {isBanner ? <Banners /> : null}
+          <Switch>
+            <Route path="/form" component={Form} />
+            <Route exact path="/movie" component={Cards} />
+            <Route exact path="/theatres" component={Theatres} />
+            <Route exact path="/event" component={Events} />
+            <Route path="/" component={Cards} />
+          </Switch>
+          <Footer title="footer menu" />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
---
+++ src/Store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const initialState = {};
+const middleware = [thunk];
+const Store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default Store;
